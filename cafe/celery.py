4d12313df02b4f75c7caf880db65fd25627bce4d
IsidroPelli <isidropelliemir@gmail.com>
2023-04-19 23:02:47 -0300

    adding docker config

---
+++ cafe/celery.py
@@ -0,0 +1,23 @@
+"""
+https://docs.celeryq.dev/en/stable/django/first-steps-with-django.html
+"""
+import os
+
+from celery import Celery
+
+from django.conf import settings
+
+# this code copied from manage.py
+# set the default Django settings module for the 'celery' app.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cafe.settings")
+
+# you can change the name here
+app = Celery("cafe")
+
+# read config from Django settings, the CELERY namespace would make celery
+# config keys has `CELERY` prefix
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# discover and load tasks.py from from all registered Django apps
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.conf.timezone = 'America/Argentina/Buenos_Aires'
