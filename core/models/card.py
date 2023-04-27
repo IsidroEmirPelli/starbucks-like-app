b21909322431f0550f1b42c1e3e72ccecc20ef05
IsidroPelli <isidropelliemir@gmail.com>
2023-04-27 02:27:35 -0300

    removing usseles things

--- core/models/card.py
+++ core/models/card.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Card(models.Model):
     number = models.CharField(max_length=19)
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     creation_date = models.DateTimeField(auto_now_add=True)
     last_use = models.DateTimeField(null=True, blank=True)
--- core/models/recharge.py
+++ core/models/recharge.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Recharge(models.Model):
     user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
-    card = models.ForeignKey("core.Card", on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     mercado_pago_data = models.JSONField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
