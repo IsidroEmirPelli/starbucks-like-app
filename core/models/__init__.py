340f4f847bb08ac7331b88324c1a57eff316c7d5
IsidroPelli <isidropelliemir@gmail.com>
2023-04-27 01:50:04 -0300

    adding recharge model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,4 +1,5 @@
 from buy import Buy
 from card import Card
 from coffee import Coffee
+from recharge import Recharge
 from user_profile import UserProfile
---
+++ core/models/recharge.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Recharge(models.Model):
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    card = models.ForeignKey("core.Card", on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    mercado_pago_data = models.JSONField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"{self.user.username} - {self.card.number} - {self.amount} - {self.date}"
+        )
+
+    class Meta:
+        verbose_name = "Recharge"
+        verbose_name_plural = "Recharges"
