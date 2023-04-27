8827a4ef7b59fe252e205dce0a9cf2f4de9ea91e
IsidroPelli <isidropelliemir@gmail.com>
2023-04-27 02:58:33 -0300

    adding campain and models to innit

---
+++ core/models/campain.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Campain(models.Model):
+    name = models.CharField(max_length=100)
+    html_content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    promotion_related = models.ForeignKey(
+        "core.Promotion", on_delete=models.CASCADE, null=True, blank=True
+    )
+    users = models.ManyToManyField("auth.User", related_name="campains")
+    objective_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name} - {self.creation_date} - {self.last_update} - {self.objective_date}"
+
+    class Meta:
+        verbose_name = "Campain"
+        verbose_name_plural = "Campains"
