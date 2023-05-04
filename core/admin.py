01caa1d06588ae5bc7f03b5f3babd16d387ba657
IsidroPelli <isidropelliemir@gmail.com>
2023-05-03 22:50:53 -0300

    adding todo to the admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from models import Buy, Campain, Card, Coffee, Promotion, recharge, UserProfile
 # Register your models here.
+
+admin.register(Buy)
+admin.register(Campain)
+admin.register(Card)
+admin.register(Coffee)
+admin.register(Promotion)
+admin.register(recharge)
+admin.register(UserProfile)
