74cb86a81425d60c2a6bdb903f9794d8cdec133e
IsidroPelli <isidropelliemir@gmail.com>
2023-05-04 01:53:30 -0300

    adding debug to env

--- cafe/settings.py
+++ cafe/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 try:
     ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
