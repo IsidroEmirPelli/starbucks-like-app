ff07c8d0a02c3656cea69a1890a4e3aa46ce71c2
IsidroPelli <isidropelliemir@gmail.com>
2023-05-10 00:00:44 -0300

    utils mp

---
+++ core/utils/mercado_pago.py
@@ -0,0 +1,27 @@
+import os
+import mercadopago
+
+def create_payment(recharge):
+    print("MercadoPago Module")
+    access_token = os.environ.get("MERCADOPAGO_ACCESS")
+    sdk = mercadopago.SDK(access_token)
+    payment_data = {
+    "transaction_amount": float(recharge.amount),
+    "token": recharge.token,
+    "description": "Recarga de saldo",
+    "installments": recharge.installments,
+    "payment_method_id": recharge.payment_method_id,
+    "payer": {
+        "email": recharge.user.email,
+        "identification": {
+            "type": recharge.identification_type,
+            "number": recharge.identification_number,
+            }
+        }
+    }
+
+
+    payment_response = sdk.payment().create(payment_data)
+    payment = payment_response["response"]
+
+    return payment
